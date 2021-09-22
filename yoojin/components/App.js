7740ec069580707ec655bb3497617588a29a7b62
daisy_yoojin <daisy.park95@gmail.com>
2021-09-22 23:41:36 +0900

    featsetting

---
+++ yoojin/components/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { mainWrapper } from "./App.css";
+const App = () => {
+  return (
+    <div className={mainWrapper}>
+      <h1> Here is main App </h1>
+    </div>
+  );
+};
+
+export default App;
