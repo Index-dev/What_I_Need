8ea8e204e425386bd5b3fc1b0672f609a42d9e01
Eum Young Hyun <opwer032@naver.com>
2021-09-26 08:45:49 +0900

    feat: Set replay and reset button

--- younghyun/app/src/main/java/com/example/eumericano/TimerActivity.kt
+++ younghyun/app/src/main/java/com/example/eumericano/TimerActivity.kt
@@ -1,18 +1,34 @@
 package com.example.eumericano
 
-import android.content.Intent
-
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
 import android.view.View
+import com.example.eumericano.databinding.ActivityMainBinding
+import com.example.eumericano.databinding.ActivityTimerBinding
 
 class TimerActivity: AppCompatActivity() {
+
+    private var tBinding: ActivityTimerBinding? = null
+    private val timerBinding get() = tBinding!!
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_timer)
+        tBinding= ActivityTimerBinding.inflate(layoutInflater)
+        setContentView(timerBinding.root)
+
+        val timerCountSettingLayer =  timerBinding.timeCountSettingLV
+        val timerCountLayer =  timerBinding.timeCountLV
+
+        val hourET = timerBinding.hourET
+        val minuteET = timerBinding.minuteET
+        val secondET = timerBinding.secondET
+
+        val hourTV = timerBinding.hourTV
+        val minuteTV = timerBinding.minuteTV
+        val secondTV = timerBinding.secondTV
+
+
     }
 
     fun handleComplete(v: View) {
