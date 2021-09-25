bfa9d21e4e9b0dd3262a83a39f45ec9b0fe5509e
Eum Young Hyun <opwer032@naver.com>
2021-09-25 15:56:35 +0900

    feat: Init main binding

--- younghyun/app/src/main/java/com/example/eumericano/MainActivity.kt
+++ younghyun/app/src/main/java/com/example/eumericano/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.eumericano
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.eumericano.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var mBinding: ActivityMainBinding? = null
+    private val binding get() = mBinding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mBinding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
+
 }
