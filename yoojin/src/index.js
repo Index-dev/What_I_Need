44f94386c50f5fa6820f9b2e10c28eb9a477b0e1
Daisy Park <daisy.park95@gmail.com>
2021-10-01 17:26:10 +0900

    feat: set style(global style) with styled-components

--- yoojin/src/index.js
+++ yoojin/src/index.js
@@ -3,10 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ThemeProvider } from 'styled-components';
+import theme from './style/theme';
+import GlobalStyle from './style/global';
+
 
 ReactDOM.render(
   <React.StrictMode>
+    <ThemeProvider theme={theme}>
     <App />
+    <GlobalStyle/>
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
