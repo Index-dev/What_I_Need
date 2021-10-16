60fb818dd5f0d0e402a3e071b1637ca624dc91f1
Eum Young Hyun <opwer032@naver.com>
2021-10-16 20:57:24 +0900

    feat: Add turn on app data

--- younghyun/app/src/main/java/com/example/eumericano/MainActivity.kt
+++ younghyun/app/src/main/java/com/example/eumericano/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.eumericano
 
+import android.content.ContentValues
 import android.content.Intent
 import android.database.sqlite.SQLiteDatabase
 import androidx.appcompat.app.AppCompatActivity
@@ -8,6 +9,8 @@ import android.view.View
 import com.example.eumericano.databinding.ActivityMainBinding
 import layout.CheckDBHelper
 import layout.MainDBHelper
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 class MainActivity : AppCompatActivity() {
     private var mBinding: ActivityMainBinding? = null
@@ -28,10 +31,17 @@ class MainActivity : AppCompatActivity() {
 
         mainDBHelper =  MainDBHelper(this, "TURN_ON_APP.db", null, 1)
         mainDatabase = mainDBHelper.writableDatabase
-        checkDBHelper =  CheckDBHelper(this, "CHECK_TODO.db", null, 1)
+        checkDBHelper =  CheckDBHelper(this, "TURN_ON_APP.db", null, 1)
         checkDatabase = checkDBHelper.writableDatabase
 
+        val current = LocalDateTime.now()
+        val formatter = DateTimeFormatter.ISO_DATE
+        val formatted = current.format(formatter)
 
+        var contentValues = ContentValues()
+        contentValues.put("date",formatted)
+
+        mainDatabase.insert("TURNED_ON_APP", null, contentValues)
     }
 
     fun showTimer(v: View) {
