e36bea336cf0606dd810458f68338a1e57a3649d
Eum Young Hyun <opwer032@naver.com>
2021-10-17 09:16:05 +0900

    feat: Add check yesterday meditation button

--- younghyun/app/src/main/java/com/example/eumericano/MainActivity.kt
+++ younghyun/app/src/main/java/com/example/eumericano/MainActivity.kt
@@ -3,9 +3,11 @@ package com.example.eumericano
 import android.content.ContentValues
 import android.content.Intent
 import android.database.sqlite.SQLiteDatabase
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.annotation.RequiresApi
 import com.example.eumericano.databinding.ActivityMainBinding
 import layout.MainDBHelper
 import java.time.LocalDateTime
@@ -16,12 +18,12 @@ class MainActivity : AppCompatActivity() {
     private val binding get() = mBinding!!
 
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         lateinit var DBHelper : MainDBHelper
         lateinit var Database : SQLiteDatabase
-
 
         mBinding= ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -44,6 +46,28 @@ class MainActivity : AppCompatActivity() {
         startActivity(intent)
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun yesterdayMeditation(v: View) {
+        lateinit var DBHelper : MainDBHelper
+        lateinit var Database : SQLiteDatabase
+        DBHelper =  MainDBHelper(this, "TURN_ON_APP.db", null, 1)
+        Database = DBHelper.writableDatabase
 
+
+        val current = LocalDateTime.now()
+        val formatter = DateTimeFormatter.ISO_DATE
+        val formattedDate = current.format(formatter)
+        val formatterTime = DateTimeFormatter.ISO_TIME
+        val formattedTime = current.format(formatterTime)
+
+
+        var contentValues = ContentValues()
+        contentValues.put("date",formattedDate)
+        contentValues.put("btn_name","yesterdayMeditation")
+        contentValues.put("time",formattedTime)
+
+
+        Database.insert("Check_TODO", null, contentValues)
+    }
 
 }
